62b61895c5c9e8aea083c2010837efc3ac6c394f
Angie Roach <angieroach4@gmail.com>
2020-01-01 11:27:16 -0500

    worked out bg colors

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-    function parallax(){
+$(document).ready(function () {
+
+    function parallax() {
         let prlxLyr2 = document.getElementById("prlxLyr1");
         let prlxLyr1 = document.getElementById("prlxLyr2");
         let prlxLyr3 = document.getElementById("prlxLyr1");
         let prlxLyr4 = document.getElementById("prlxLyr2");
-        prlxLyr1.style.top = (window.pageYOffset/8) + "px";
-        prlxLyr2.style.top = (window.pageYOffset/10) + "px";
-        prlxLyr3.style.top = (window.pageYOffset/8) + "px";
-        prlxLyr4.style.top = (window.pageYOffset/10) + "px";
+        prlxLyr1.style.top = (window.pageYOffset / 15) + "px";
+        prlxLyr2.style.top = (window.pageYOffset / 20) + "px";
+        prlxLyr3.style.top = (window.pageYOffset / 15) + "px";
+        prlxLyr4.style.top = (window.pageYOffset / 20) + "px";
     }
 });
-
-window.addEventListener("scroll", parallax, false);
