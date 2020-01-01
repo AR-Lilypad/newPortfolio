b1f693464853a06b36c569104708d228af7dab73
Angie Roach <angieroach4@gmail.com>
2019-12-31 22:29:53 -0500

    page updates incl: sections, cards

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,13 @@
 $(document).ready(function() {
     function parallax(){
-        let prlxLyr1 = document.getElementById("prlxLyr1");
-        let prlxLyr2 = document.getElementById("prlxLyr2");
+        let prlxLyr2 = document.getElementById("prlxLyr1");
+        let prlxLyr1 = document.getElementById("prlxLyr2");
+        let prlxLyr3 = document.getElementById("prlxLyr1");
+        let prlxLyr4 = document.getElementById("prlxLyr2");
         prlxLyr1.style.top = (window.pageYOffset/8) + "px";
-        prlxLyr2.style.top = (window.pageYOffset/8) + "px";
+        prlxLyr2.style.top = (window.pageYOffset/10) + "px";
+        prlxLyr3.style.top = (window.pageYOffset/8) + "px";
+        prlxLyr4.style.top = (window.pageYOffset/10) + "px";
     }
 });
 
